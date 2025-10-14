e69a8075f18a3e0d89a233cfb0a7ef2212a96e95
Bijay kumar shah <bjsah3277@gmail.com>
2025-10-14 22:14:50 +0530

    Implement Navbar and HeroSection components with responsive design

--- E-Blog_front/src/App.jsx
+++ E-Blog_front/src/App.jsx
@@ -2,39 +2,29 @@ import React from 'react'
 import Login from './Pages/Login'
 import Signup from './Pages/Signup'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-<<<<<<< HEAD
-=======
 import Navbar from './Components/Navbar'
-<<<<<<< HEAD
 import HeroSection from './Components/HeroSection'
 import "./App.css"
-=======
->>>>>>> 339965d96da937d2587d5c9c92ed5377cdc93ca0
 import './App.css'
-
->>>>>>> b0acc7bc1cfd17f2555035dfaf7d71010e29f3de
 
 function App() {
   return (
-<<<<<<< HEAD
     <>
-      <Navbar/>
-      <HeroSection/>
-    </>
-=======
-    <div>
-      <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Login/>}/>
-        <Route path='/signup' element={<Signup/>}/>
-      </Routes>
-      </BrowserRouter>
-       
-    </div>
+      <Navbar />
+      <HeroSection />
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Login />} />
+            <Route path='/signup' element={<Signup />} />
+          </Routes>
+        </BrowserRouter>
 
->>>>>>> b0acc7bc1cfd17f2555035dfaf7d71010e29f3de
+      </div>
+
+    </>
   )
 }
-   
+
 
 export default App
