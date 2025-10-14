130f197fb32440587a81d1dc2ba795946ab31549
Ronitraj95 <149725406+Ronitraj95@users.noreply.github.com>
2025-10-14 15:30:38 +0530

    Hero Section

--- E-Blog_front/src/App.jsx
+++ E-Blog_front/src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from './Components/Navbar'
+import HeroSection from './Components/HeroSection'
 import "./App.css"
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
   return (
     <>
       <Navbar/>
-       
+      <HeroSection/>
     </>
   )
 }
