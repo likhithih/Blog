c616d00c701f7328572c4deca28e1906e13c23a0
likhithih <likhithih2003@gmail.com>
2025-10-15 15:15:51 +0530

    hero section pushed

--- E-Blog_front/src/App.jsx
+++ E-Blog_front/src/App.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './Components/Navbar'
-import HeroSection from './Components/HeroSection'
-import Login from './Pages/Login'
-import Signup from './Pages/Signup'
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import HeroSection from "./Components/HeroSection";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+import Card from "./Components/Card";
 
 function App() {
   return (
-    <div>
-        <Navbar/>
+    <>
+      <div>
+        <Navbar />
+        <HeroSection />
+        <Card />
+      </div>
+      <div>
         <Routes>
-          <Route path="/" element={<HeroSection />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
-    </div>
-  )
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
