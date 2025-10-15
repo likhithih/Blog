55e047305e7cd7f00f2f0b08a57869a4f0541836
Ronitraj95 <149725406+Ronitraj95@users.noreply.github.com>
2025-10-15 15:20:03 +0530

    likhith

--- E-Blog_front/src/App.jsx
+++ E-Blog_front/src/App.jsx
@@ -1,29 +1,30 @@
 import React from 'react'
-import Login from './Pages/Login'
-import Signup from './Pages/Signup'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import Login from './Pages/Login'
+// import Signup from './Pages/Signup'
+// //import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './Components/Navbar'
-
 import HeroSection from './Components/HeroSection'
 import "./App.css"
+import ReviewSection from './Components/ReviewSection'
+import Footer from './Components/Footer'
 
-
-import './App.css'
- b0acc7bc1cfd17f2555035dfaf7d71010e29f3de
+ 
 
 function App() {
   return (
     <>
-      <Navbar />
-      <HeroSection />
-      <div>
+      {/* <Navbar />
+      <HeroSection /> */}
+      <ReviewSection/>
+      <Footer/>
+      {/* <div>
         <BrowserRouter>
           <Routes>
             <Route path='/' element={<Login />} />
             <Route path='/signup' element={<Signup />} />
           </Routes>
         </BrowserRouter>
- </div>
+      </div> */}
 
       </>       
   )
