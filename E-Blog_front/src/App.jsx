d00677be80939a1079c95f1d3a60eef259a26be5
likhithih <likhithih2003@gmail.com>
2025-10-15 14:24:15 +0530

    dont change app.js

--- E-Blog_front/src/App.jsx
+++ E-Blog_front/src/App.jsx
@@ -5,39 +5,17 @@ import Navbar from './Components/Navbar'
 import HeroSection from './Components/HeroSection'
 import Login from './Pages/Login'
 import Signup from './Pages/Signup'
-<<<<<<< HEAD
 
 function App() {
   return (
     <div>
-        {/* <Navbar/> */}
+        <Navbar/>
         <Routes>
           <Route path="/" element={<HeroSection />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
     </div>
-=======
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Navbar from './Components/Navbar'
-
-import HeroSection from './Components/HeroSection'
-import "./App.css"
-
-function App() {
-  return (
-    <>
-      <div>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Login />} />
-            <Route path='/signup' element={<Signup />} />
-          </Routes>
-        </BrowserRouter>
- </div>
-
-      </>       
->>>>>>> 963d962e5b5994b6ed27654dfd74881b6abbe29c
   )
 }
 
