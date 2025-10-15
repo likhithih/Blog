885a08d8df0cabfce3bc4d9fbf5ec695cc05bc1c
likhithih <likhithih2003@gmail.com>
2025-10-15 11:57:50 +0530

    altered hero

--- E-Blog_front/src/Components/HeroSection.jsx
+++ E-Blog_front/src/Components/HeroSection.jsx
@@ -8,11 +8,6 @@ const HeroSection = () => {
         <div style={{ clipPath: "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" }} className="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"></div>
       </div>
       <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-        <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-          <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-            Announcing our latest blog post. <a href="#" className="font-semibold text-indigo-600"><span aria-hidden="true" className="absolute inset-0"></span>Read more <span aria-hidden="true">&rarr;</span></a>
-          </div>
-        </div>
         <div className="text-center">
           <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">Welcome to a world of learning!</h1>
           <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">A professional blog sharing the latest in technology, web design, and digital innovation — helping you turn ideas into impact.</p>
