ccbcaeaa8ec435e9001ff164c6eb1c9e14ae101b
Ronitraj95 <149725406+Ronitraj95@users.noreply.github.com>
2025-10-18 15:27:59 +0530

    footer icon change

--- E-Blog_front/src/Components/Footer.jsx
+++ E-Blog_front/src/Components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import logo from '../assets/Favicon.png'
 
 const Footer = () => {
   return (
@@ -8,7 +9,7 @@ const Footer = () => {
           {/* Company Info */}
           <div className="space-y-4">
             <div className="flex items-center">
-              <svg
+              {/* <svg
                 className="h-8 w-8 text-blue-400"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -20,7 +21,8 @@ const Footer = () => {
                   strokeWidth="2"
                   d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"
                 />
-              </svg>
+              </svg> */}
+              <span><img src={logo} alt=""  width='40px'/></span>
               <span className="ml-2 text-xl font-bold">E-Blogs</span>
             </div>
             <p className="text-gray-400">
