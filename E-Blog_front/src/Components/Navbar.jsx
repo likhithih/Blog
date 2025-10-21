3124edbf3648e7a24b8e98b4a432d172d2101edf
Ronitraj95 <149725406+Ronitraj95@users.noreply.github.com>
2025-10-21 16:04:06 +0530

    Chnages logo

--- E-Blog_front/src/Components/Navbar.jsx
+++ E-Blog_front/src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import logo from '../assets/Blog-logo-Nav.png';
+import logo from '../assets/Logos/E-Blog.png';
 import { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
